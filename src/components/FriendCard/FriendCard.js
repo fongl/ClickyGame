b0830275507de3eb268c63c5a50bc236d90278e1
Lewis Fong <fongl@bu.edu>
2018-04-12 11:27:10 -0700

    relative path

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -4,7 +4,7 @@ import "./FriendCard.css";
 const FriendCard = props => (
   <div className="card" >
     <div className="img-container" onClick={() => props.activation(props.id)}>
-      <img alt={props.name} src={props.image}  />
+      <img alt={props.name} src={window.location.origin+props.image}  />
     </div>
   </div>
 );
